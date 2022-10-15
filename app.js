41bf77a4ec1254d1add117daa2ab634bb1d5ff79
Daniel151505 <daniel150520011@gmail.com>
2022-10-14 23:02:34 -0500

    Estado del Componente

--- app.js
+++ app.js
@@ -3,6 +3,14 @@ const app = Vue.createApp({
     // <h1> Hola Mundo </h1>
     // <p> Desde app.js </p>
     // `
+
+    data() {
+        return {
+            quote: "I'm Spiderman",
+            autor: "Peter Parker"
+        }
+    },
+
 })
 
 app.mount('#myApp')
